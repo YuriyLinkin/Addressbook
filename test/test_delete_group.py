5f4ec95327dd3afe8d9a25b8a7718e5fc826d4b0
yuriy <yb150187@gmail.com>
2017-04-22 19:02:14 +0300

    Fixing and assert filesMessage

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -4,5 +4,6 @@ def test_delete_group(app):
    app.open_page()
    app.delete_group_by_number(0)
    # TODO: Verify message
+   assert "Group has been removed." in app.message()
    app.return_group_page()
    # TODO: Deletion group in list
